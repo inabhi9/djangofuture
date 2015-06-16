04d2bbde1e776f17e826e2e18339d8255775d46e
Abhinav <in.abhi9@gmail.com>
2015-06-17 02:19:33 +0530

    array, hstore and rages fields from django

--- djangofuture/contrib/postgres/fields/__init__.py
+++ djangofuture/contrib/postgres/fields/__init__.py
@@ -1,4 +1,4 @@
-from .array import *  # NOQA
-from .hstore import *  # NOQA
+from django.contrib.postgres.fields.array import *  # NOQA
+from django.contrib.postgres.fields.hstore import *  # NOQA
 from .jsonb import *  # NOQA
-from .ranges import *  # NOQA
+from django.contrib.postgres.fields.ranges import *  # NOQA
